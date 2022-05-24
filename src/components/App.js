72ad73d681fc4211bae334c80f9e2dcef06f3af3
Valentyn Digtiarenko <baggins999@gmail.com>
2022-05-24 23:09:22 +0300

    6

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import user from './SocialProfile/user.json';
+import Profile from './SocialProfile/Profile';
+
+export const App = () => {
+  return (
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    </div>
+  );
+};
---
+++ src/components/SocialProfile/Profile.jsx
@@ -0,0 +1,38 @@
+import s from './Profile.module.css';
+import PropTypes from 'prop-types';
+
+const Profile = ({
+  username,
+  tag,
+  location,
+  avatar,
+  stats: { followers, views, likes },
+}) => {
+  return (
+    <div className={s.profile}>
+      <div className={s.description}>
+        <img src={avatar} alt="User avatar" className={s.avatar} />
+        <p className={s.name}>{username}</p>
+        <p className={s.location}>{tag}</p>
+        <p className={s.location}>{location}</p>
+      </div>
+
+      <ul className={s.stats}>
+        <li>
+          <span className={s.label}>Followers</span>
+          <span className={s.quantity}>{followers}</span>
+        </li>
+        <li>
+          <span className={s.label}>Views</span>
+          <span className={s.quantity}>{views}</span>
+        </li>
+        <li>
+          <span className={s.label}>Likes</span>
+          <span className={s.quantity}>{likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Profile;
