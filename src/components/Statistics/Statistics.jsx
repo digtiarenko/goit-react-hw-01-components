922acb3ca994c734cb53ae8b8b066f31c2af1bf9
Valentyn Digtiarenko <baggins999@gmail.com>
2022-05-27 12:19:58 +0300

    Ten

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -4,7 +4,7 @@ import s from './statistics.module.css';
 const Statistics = ({ title, stats }) => {
   return (
     <section className={s.statistics}>
-      {title ? <h2 className={s.title}>{title}</h2> : ''}
+      {title ?? <h2 className={s.title}>{title}</h2>}
 
       <ul className={s.statList}>
         {stats.map(({ id, label, percentage }) => {
